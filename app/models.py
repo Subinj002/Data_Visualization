45f02ca455f7db3b173323219e348e73c38dd833
Subinj002 <subinjosephchethipuzha@gmail.com>
2025-10-29 14:42:10 +0530

    Delete app directory

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-class Inventory(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    item_name = db.Column(db.String(100))
-    quantity = db.Column(db.Integer)
-    category = db.Column(db.String(50))
-    last_updated = db.Column(db.DateTime)
